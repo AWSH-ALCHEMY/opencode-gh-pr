8e33980b7e083b9f936281363cc8e48fc9eb502f
code@awsh.net <code@awsh.net>
2026-03-21 14:52:22 -0400

    refactor: remove local action pre and post hooks

--- src/cleanup.ts
+++
@@ -1,5 +0,0 @@
-// This script runs after the main action has completed
-
-import * as core from '@actions/core';
-
-core.info('🧹 Secure PR Review action cleanup complete');
--- src/setup.ts
+++
@@ -1,5 +0,0 @@
-// This script runs before the main action
-
-import * as core from '@actions/core';
-
-core.info('🚀 Starting Secure PR Review action setup');
